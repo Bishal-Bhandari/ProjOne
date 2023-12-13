2c1f0b38ce4f696b289d2bc33ee135019170bc3a
bishal <bishalbhaandari62@gmail.com>
2023-12-13 13:19:14 +0100

    Simple API

--- src/main/java/com/example/ProjOne/ProjOneApplication.java
+++ src/main/java/com/example/ProjOne/ProjOneApplication.java
@@ -1,9 +1,17 @@
 package com.example.ProjOne;
 
+import com.example.ProjOne.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class ProjOneApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +19,13 @@ public class ProjOneApplication {
 		SpringApplication.run(ProjOneApplication.class, args);
 	}
 
+	@GetMapping
+	public List<Student> Say(){
+		return List.of(
+			new Student(
+					1L, "Bishal", "bishal@gmail.com", LocalDate.of(1995, Month.OCTOBER, 31),
+					28
+			)
+		);
+	}
 }
