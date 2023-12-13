9e319dfcb9fa7044e5908b85deae023d6ecc6e94
bishal <bishalbhaandari62@gmail.com>
2023-12-13 12:35:09 +0100

    init

---
+++ src/main/java/com/example/ProjOne/ProjOneApplication.java
@@ -0,0 +1,14 @@
+package com.example.ProjOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjOneApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjOneApplication.class, args);
+	}
+
+}
