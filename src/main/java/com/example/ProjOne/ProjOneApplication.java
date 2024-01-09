16eeb685817796c4f53de75334a1c0a1edcc50a0
bishal <bishalbhaandari62@gmail.com>
2024-01-09 10:23:36 +0100

    -req mapping added -controller added

--- src/main/java/com/example/ProjOne/ProjOneApplication.java
+++ src/main/java/com/example/ProjOne/ProjOneApplication.java
@@ -1,31 +1,13 @@
 package com.example.ProjOne;
 
-import com.example.ProjOne.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class ProjOneApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(ProjOneApplication.class, args);
-	}
-
-	@GetMapping
-	public List<Student> Say(){
-		return List.of(
-			new Student(
-					1L, "Bishal", "bishal@gmail.com", LocalDate.of(1995, Month.OCTOBER, 31),
-					28
-			)
-		);
 	}
 }
---
+++ src/main/java/com/example/ProjOne/Student/StudentController.java
@@ -0,0 +1,23 @@
+package com.example.ProjOne.Student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    @GetMapping
+    public List<Student> Say(){
+        return List.of(
+                new Student(
+                        1L, "Bishal", "bishal@gmail.com", LocalDate.of(1995, Month.OCTOBER, 31),
+                        28
+                )
+        );
+    }
+}
