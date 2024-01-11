9a8aefa21df92842f8bbd8a89cc03b24745c8cce
bishal <bishalbhaandari62@gmail.com>
2024-01-11 12:14:36 +0100

    -INTERFACE ADDED -Repo added
    -

--- src/main/java/com/example/ProjOne/Student/StudentService.java
+++ src/main/java/com/example/ProjOne/Student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.ProjOne.Student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,13 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
         return List.of(
                 new Student(
